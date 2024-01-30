3ddeb8fb5bff1609712a15b7ea088d6b85de5114
SantosVicente <santos.vicente@aluno.ifsp.edu.br>
2024-01-29 22:38:43 -0300

    chore: init shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="w-screen h-screen flex justify-center items-center bg-zinc-950">
+    <div className="w-screen h-screen flex justify-center items-center">
       <h1>FSW Barber</h1>
     </div>
   );
