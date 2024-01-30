208a65eeb366296d5c6bf59fbed236cc1bebfb00
SantosVicente <santos.vicente@aluno.ifsp.edu.br>
2024-01-29 22:43:26 -0300

    chore: set theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
+    <div className="w-screen h-screen flex-col space-y-4 flex justify-center items-center">
       <h1>FSW Barber</h1>
+      <Button variant={"default"}>Click me</Button>
     </div>
   );
 }
